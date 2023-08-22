1bb154f65d1b2d7007ff129b74401f4622dffa71
Frederico Reckziegel <freddalsoglio@gmail.com>
2023-08-22 20:49:47 -0300

    adiciona app projects

--- abrace/urls.py
+++ abrace/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', include('projects.urls'))
 ]
